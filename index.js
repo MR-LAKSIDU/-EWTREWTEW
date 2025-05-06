c5e03defe79e7d25414e181e0e69c485b1a7f802
MR-LAKSIDU <laksidunimsara123456781@gmail.com>
2025-05-06 10:05:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94724400710']
+const ownerNumber = ['94771470396']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
